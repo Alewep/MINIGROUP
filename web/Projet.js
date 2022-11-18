cae77bd112091489a54ccac00b235e500d72e83f
etud user <etud@port-dell-precision-7520-27>
2022-11-18 16:46:25 +0100

    ajout de la modification des listes instruments, villes et styles

--- web/Projet.js
+++ web/Projet.js
@@ -1,8 +1,174 @@
 var i=0;
 
-function ajoutLigne(){
-    ++i;
+var listeInstru = ["Guitare", "Batterie", "Violon", "Piano", "Basse"];
+var listeVilles = ["Angers", "Nantes"];
+var listeStyles = ["Rock", "Metal", "Jazz", "Pop", "Classique", "Funk", "RnB", "Rap", "Electro", "Punk"];
 
+function ajoutInstru(){
+    let instru = document.getElementById("ajoutInstru").value;
+    listeInstru.push(instru);
+   
+    affichageInstru();
+    ChangementTableauInstru();
+}
+
+function affichageInstru(){
+    let tr = document.getElementById("ligneInstru");
+    let td = document.getElementById("Instru");
+    tr.removeChild(td);
+    
+    td = document.createElement('td');
+    td.setAttribute("id", "Instru");
+
+    for(let j = 0; j < listeInstru.length; ++j){
+        let button = document.createElement('input');
+        button.setAttribute("type", "button");
+        button.setAttribute("value", listeInstru[j]);
+        button.setAttribute("onclick", "supprimerInstru("+j+")");
+        td.append(button);
+    }
+
+    tr.append(td);
+}
+
+function supprimerInstru(j){
+    listeInstru.splice(j, 1);
+    affichageInstru();
+    ChangementTableauInstru();
+}
+
+function ChangementTableauInstru(){
+    for(j = 0; j < i; ++j){
+        let td = document.getElementById("ligneInstru"+j);
+        let select = document.getElementById("Instru"+j);
+        td.removeChild(select);
+
+        select = document.createElement('select');
+        select.setAttribute("name", "Instru");
+        select.setAttribute("id", "Instru"+j);
+
+        for(let k = 0; k < listeInstru.length; ++k){
+            let option = document.createElement('option');
+            option.setAttribute("value", listeInstru[k]);
+            let instru = document.createTextNode(listeInstru[k]);
+            option.append(instru);
+            select.append(option);
+        }
+        td.append(select);
+    }
+}
+
+function ajoutVille(){
+    let ville = document.getElementById("ajoutVille").value;
+    listeVilles.push(ville);
+   
+    affichageVille();
+    ChangementTableauVille();
+}
+
+function affichageVille(){
+    let tr = document.getElementById("ligneVille");
+    let td = document.getElementById("Villes");
+    tr.removeChild(td);
+    
+    td = document.createElement('td');
+    td.setAttribute("id", "Villes");
+
+    for(let j = 0; j < listeVilles.length; ++j){
+        let button = document.createElement('input');
+        button.setAttribute("type", "button");
+        button.setAttribute("value", listeVilles[j]);
+        button.setAttribute("onclick", "supprimerVille("+j+")");
+        td.append(button);
+    }
+
+    tr.append(td);
+}
+
+function supprimerVille(j){
+    listeVilles.splice(j, 1);
+    affichageVille();
+    ChangementTableauVille();
+}
+
+function ChangementTableauVille(){
+    for(j = 0; j < i; ++j){
+        let td = document.getElementById("ligneVille"+j);
+        let select = document.getElementById("Ville"+j);
+        td.removeChild(select);
+
+        select = document.createElement('select');
+        select.setAttribute("name", "Ville");
+        select.setAttribute("id", "Ville"+j);
+
+        for(let k = 0; k < listeVilles.length; ++k){
+            let option = document.createElement('option');
+            option.setAttribute("value", listeVilles[k]);
+            let ville = document.createTextNode(listeVilles[k]);
+            option.append(ville);
+            select.append(option);
+        }
+        td.append(select);
+    }
+}
+
+function ajoutStyles(){
+    let style = document.getElementById("ajoutStyles").value;
+    listeStyles.push(style);
+   
+    affichageStyles();
+    ChangementTableauStyles();
+}
+
+function affichageStyles(){
+    let tr = document.getElementById("ligneStyles");
+    let td = document.getElementById("Styles");
+    tr.removeChild(td);
+    
+    td = document.createElement('td');
+    td.setAttribute("id", "Styles");
+
+    for(let j = 0; j < listeStyles.length; ++j){
+        let button = document.createElement('input');
+        button.setAttribute("type", "button");
+        button.setAttribute("value", listeStyles[j]);
+        button.setAttribute("onclick", "supprimerStyles("+j+")");
+        td.append(button);
+    }
+
+    tr.append(td);
+}
+
+function supprimerStyles(j){
+    listeStyles.splice(j, 1);
+    affichageStyles();
+    ChangementTableauStyles();
+}
+
+function ChangementTableauStyles(){
+    for(j = 0; j < i; ++j){
+        let td = document.getElementById("ligneStyles"+j);
+        let select = document.getElementById("Styles"+j);
+        td.removeChild(select);
+
+        select = document.createElement('select');
+        select.setAttribute("name", "Styles");
+        select.setAttribute("id", "Styles"+j);
+        select.setAttribute("multiple", "true");
+
+        for(let k = 0; k < listeStyles.length; ++k){
+            let option = document.createElement('option');
+            option.setAttribute("value", listeStyles[k]);
+            let style = document.createTextNode(listeStyles[k]);
+            option.append(style);
+            select.append(option);
+        }
+        td.append(select);
+    }
+}
+
+function ajoutLigne(){
+    
     var tbody = document.getElementById("tbody");
 
     var tr = document.createElement('tr');
@@ -32,121 +198,49 @@ function ajoutLigne(){
     td.append(input);
     tr.append(td);
 
+    /* Les instrments */
     var td = document.createElement('td');
+    td.setAttribute("id", "ligneInstru"+i);
     var select = document.createElement('select');
     select.setAttribute("id", "Instru"+i);
     select.setAttribute("name", "Instru");
-    var option = document.createElement('option');
-    option.setAttribute("value", "Guitare");
-    var texte = document.createTextNode("Guiatre");
-    option.append(texte);
-    select.append(option);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Batterie");
-    var texte = document.createTextNode("Batterie");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Violon");
-    var texte = document.createTextNode("Vilion");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Piano");
-    var texte = document.createTextNode("Piano");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Basse");
-    var texte = document.createTextNode("Basse");
-    option.append(texte);
-    select.append(option);
+    for(let j = 0; j < listeInstru.length; ++j){
+        var option = document.createElement('option');
+        option.setAttribute("value", listeInstru[j]);
+        option.append(listeInstru[j]);
+        select.append(option);
+    }
     td.append(select);
     tr.append(td);
 
+    /* Les villes */
     var td = document.createElement('td');
+    td.setAttribute("id", "ligneVille"+i)
     var select = document.createElement('select');
     select.setAttribute("id", "Ville"+i);
     select.setAttribute("name", "Ville");
-    var option = document.createElement('option');
-    option.setAttribute("value", "Angers");
-    var texte = document.createTextNode("Angers");
-    option.append(texte);
-    select.append(option);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Nantes");
-    var texte = document.createTextNode("Nantes");
-    option.append(texte);
-    select.append(option);
+    for(let j = 0; j < listeVilles.length; ++j){
+        var option = document.createElement('option');
+        option.setAttribute("value", listeVilles[j]);
+        option.append(listeVilles[j]);
+        select.append(option);
+    }
     td.append(select);
     tr.append(td);
 
+    /* les styles */
     var td = document.createElement('td');
+    td.setAttribute("id", "ligneStyles"+i);
     var select = document.createElement('select');
     select.setAttribute("id", "Styles"+i);
     select.setAttribute("name", "Styles");
-    select.setAttribute("multiple", "true")
-    var option = document.createElement('option');
-    option.setAttribute("value", "Rock");
-    var texte = document.createTextNode("Rock");
-    option.append(texte);
-    select.append(option);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Metal");
-    var texte = document.createTextNode("Metal");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Jazz");
-    var texte = document.createTextNode("Jazz");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Pop");
-    var texte = document.createTextNode("Pop");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Classique");
-    var texte = document.createTextNode("Classique");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Funk");
-    var texte = document.createTextNode("Funk");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "RnB");
-    var texte = document.createTextNode("RnB");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Rap");
-    var texte = document.createTextNode("Rap");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Eectro");
-    var texte = document.createTextNode("Electro");
-    option.append(texte);
-    select.append(option);
-    td.append(select);
-    var option = document.createElement('option');
-    option.setAttribute("value", "Punk");
-    var texte = document.createTextNode("Punk");
-    option.append(texte);
-    select.append(option);
+    select.setAttribute("multiple", "true");
+    for(let j = 0; j < listeStyles.length; ++j){
+        var option = document.createElement('option');
+        option.setAttribute("value", listeStyles[j]);
+        option.append(listeStyles[j]);
+        select.append(option);
+    }
     td.append(select);
     tr.append(td);
 
@@ -191,4 +285,12 @@ function ajoutLigne(){
     tr.append(td);
 
     tbody.append(tr);
+
+    ++i;
 }
+
+ajoutLigne();
+
+affichageInstru();
+affichageVille();
+affichageStyles();
